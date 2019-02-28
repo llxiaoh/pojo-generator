81e40c8b985928c00ba1b6ce0d2464899c88b876
gukun <644932921@qq.com>
2019-03-01 07:49:00 +0800

    部分提交

--- main.py
+++ main.py
@@ -2,26 +2,25 @@
 import codecs
 import os
 import pymysql
+import traceback
 
 
 class DATA_BASE_CONFIG:
-    URL=""
+    HOST = ""
+    DB = ""
     USERNAME = ""
     PASSWORD = ""
     # 是否开启驼峰写法
     CAMEL_STYLE=True
     TARGET_TABLE_NAME = ""
+    GET_TABLE_FIELDS_SQL = "show full fields from %s";
 
 class Field:
-
     column_name = None;
-
     def __init__(self,column_name):
         self.column_name = column_name
-
     def get_column_name(self):
         return self.column_name
-
     def get_property_name(self):
         if self.column_name is None:
             raise Exception("字段未赋值")
@@ -30,31 +29,46 @@ class Field:
         return self.column_name
 
 class TableData:
-    # 表名
     table_name = None
-    # 字段列表
     field_list = None
-    # 包名
     package_name = None
-
     def __init__(self,table_name):
         self.table_name = table_name
-
     def add_column(self,field):
         if self.field_list is None:
             self.field_list = []
         self.field_list.append(field)
 
-
 def getConnection():
-    pass
+    return pymysql.connect(host=DATA_BASE_CONFIG.HOST,
+                           db=DATA_BASE_CONFIG.DB,
+                           user=DATA_BASE_CONFIG.USERNAME,
+                           passwd=DATA_BASE_CONFIG.PASSWORD,
+                           charset="utf8",
+                           use_unicode=True,)
 
 def fillAllTaleData(tablenameList,tableData):
+    try:
+        connection = getConnection()
+        cursor = connection.cursor()
+        cursor.execute(DATA_BASE_CONFIG.GET_TABLE_FIELDS_SQL)
+        result = cursor.fetchall()
+        if result is None:
+            raise Exception("获取表字段异常")
+        for field_row in result:
+            print(field_row[0])
+    except:
+        traceback.print_exc()
     pass
 
 def generateTableData():
     pass
 
+def generate_java_pojo():
+    pass
+
+def generate_mybatis_sql():
+    pass
 
 if __name__ == "__main__":
     print("START GENERATOR ...")
